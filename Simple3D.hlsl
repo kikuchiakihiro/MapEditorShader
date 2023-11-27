7be5c50bb6e268eac85559de62be8d37e165bf76
明宏 菊池 <yfcbs22yra@docomo.ne.jp>
2023-11-27 10:10:29 +0900

    鏡面反射光

--- Simple3D.hlsl
+++ Simple3D.hlsl
@@ -42,7 +42,7 @@ VS_OUT VS(float4 pos : POSITION, float4 uv : TEXCOORD, float4 normal : NORMAL)
 
 	//ñ@ê¸ÇâÒì]
 	normal = mul(normal,matW);
-	float4 light = float4(-1, 0.8, -0.3, 0);
+	float4 light = float4(-1, 1, 0, 0);
 	light = normalize(light);
 	outData.color = clamp(dot(normal, light), 0, 1);
 
